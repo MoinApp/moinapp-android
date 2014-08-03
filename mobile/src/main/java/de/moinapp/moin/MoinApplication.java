1a8dacf912540d99b8a057ebfcb0c1bbcf408132
Jan-Henrik <jh.bruhn@me.com>
2014-08-03 21:14:45 +0200

    No more Logging for the JobManager

--- mobile/src/main/java/de/moinapp/moin/MoinApplication.java
+++ mobile/src/main/java/de/moinapp/moin/MoinApplication.java
@@ -2,11 +2,9 @@ package de.moinapp.moin;
 
 import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import com.path.android.jobqueue.JobManager;
 import com.path.android.jobqueue.config.Configuration;
-import com.path.android.jobqueue.log.CustomLogger;
 
 import de.moinapp.moin.db.DaoMaster;
 import de.moinapp.moin.db.DaoSession;
@@ -37,29 +35,6 @@ public class MoinApplication extends Application {
 
     private void setupJobManager() {
         Configuration configuration = new Configuration.Builder(this)
-                .customLogger(new CustomLogger() {
-                    private static final String TAG = "JOBS";
-
-                    @Override
-                    public boolean isDebugEnabled() {
-                        return true;
-                    }
-
-                    @Override
-                    public void d(String text, Object... args) {
-                        Log.d(TAG, String.format(text, args));
-                    }
-
-                    @Override
-                    public void e(Throwable t, String text, Object... args) {
-                        Log.e(TAG, String.format(text, args), t);
-                    }
-
-                    @Override
-                    public void e(String text, Object... args) {
-                        Log.e(TAG, String.format(text, args));
-                    }
-                })
                 .minConsumerCount(1)//always keep at least one consumer alive
                 .maxConsumerCount(3)//up to 3 consumers at a time
                 .loadFactor(3)//3 jobs per consumer
