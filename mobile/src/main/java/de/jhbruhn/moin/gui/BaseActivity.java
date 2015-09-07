e61abb93b970963542db3c7c65ceb12533faf193
Jan-Henrik Bruhn <jh.bruhn@me.com>
2015-09-07 10:42:29 +0200

    Use the new AppCompatActivity

--- mobile/src/main/java/de/jhbruhn/moin/gui/BaseActivity.java
+++ mobile/src/main/java/de/jhbruhn/moin/gui/BaseActivity.java
@@ -2,6 +2,7 @@ package de.jhbruhn.moin.gui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
 import de.jhbruhn.moin.MoinApplication;
@@ -9,7 +10,7 @@ import de.jhbruhn.moin.MoinApplication;
 /**
  * Created by Jan-Henrik on 19.12.2014.
  */
-public abstract class BaseActivity extends ActionBarActivity {
+public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
